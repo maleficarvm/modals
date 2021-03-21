6f9defefd3bddc49ed96537f475a92a22b5ec913
Wladimir Muravyev <wladimir.muravyev@gmail.com>
2021-03-21 22:09:01 +0300

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <!-- first modal -->
+          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">
+            Show first modal
+          </button>
+          <modals
+            title="First modal"
+            v-show="modalFirst"
+            @close="modalFirst = false"
+          >
+            <slot>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <button class="btn btnPrimary" @click="modalFirst = !modalFirst">
+                Well done!
+              </button>
+            </slot>
+          </modals>
+          <!-- second modal -->
+          <button
+            class="btn btnPrimary"
+            @click="modalSecond.show = !modalSecond.show"
+          >
+            Show modal with form
+          </button>
+          <modals
+            title="Modal with form"
+            v-show="modalSecond.show"
+            @close="modalSecond.show = false"
+          >
+            <slot>
+              <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> -->
+              <form @submit.prevent="submitSecondForm">
+                <label>Name:</label>
+                <input type="text" required v-model="modalSecond.name" />
+                <label>Email:</label>
+                <input type="email" required v-model="modalSecond.email" />
+                <button class="btn btnPrimary">
+                  Submit!
+                </button>
+              </form>
+            </slot>
+          </modals>
+          <!-- modal with validate -->
+          <button
+            class="btn btnPrimary"
+            @click="modalValidate = !modalValidate"
+          >
+            Show modal with form + validate
+          </button>
+          <modalValidate
+            v-show="modalValidate"
+            @close="modalValidate = false"
+          />
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import modals from "@/components/UI/Modal.vue";
+import modalValidate from "@/components/ModalValidate.vue";
+export default {
+  components: { modals, modalValidate },
+  data() {
+    return {
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: "",
+        email: "",
+      },
+      modalValidate: false,
+    };
+  },
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email,
+      });
+      this.modalSecond.name = "";
+      this.modalSecond.name = "";
+      this.modalSecond.show = false;
+    },
+  },
+};
+</script>
+
+<style></style>
